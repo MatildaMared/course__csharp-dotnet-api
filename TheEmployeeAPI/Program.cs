78171eef0682a830a379b3e9d13ff7101f3ea78b
Matilda Mared <matildamared@live.se>
2024-11-23 11:23:52 +0100

    add test project

--- TheEmployeeAPI/Program.cs
+++ TheEmployeeAPI/Program.cs
@@ -1,51 +1,46 @@
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var employees = new List<Employee> {
-    new Employee { Id = 1, FirstName = "John", LastName = "Doe" },
-    new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" }
-};
+var employees = new List<Employee>();
+
+employees.Add(new Employee { Id = 1, FirstName = "John", LastName = "Doe" });
+employees.Add(new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" });
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-var employeeRoute = app.MapGroup("/employees");
+var employeeRoute = app.MapGroup("employees");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-employeeRoute.MapGet(string.Empty, () =>
+
+employeeRoute.MapGet("/", () =>
 {
     return Results.Ok(employees);
 });
 
-employeeRoute.MapGet("{id}", (int id) => 
+employeeRoute.MapGet("/{id}", (int id) =>
 {
-    var employee = employees.FirstOrDefault(e => e.Id == id);
+    var employee = employees.SingleOrDefault(e => e.Id == id);
     if (employee == null)
     {
         return Results.NotFound();
     }
-
     return Results.Ok(employee);
 });
 
-employeeRoute.MapPost(string.Empty, ([FromBody] Employee employee) =>
+employeeRoute.MapPost("/", (Employee employee) =>
 {
     employee.Id = employees.Max(e => e.Id) + 1;
     employees.Add(employee);
-
     return Results.Created($"/employees/{employee.Id}", employee);
 });
 
