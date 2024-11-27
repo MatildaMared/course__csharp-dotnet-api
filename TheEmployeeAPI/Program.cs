b58d9dcd16a9dd97c40b3f04fb67ac3e80b1d81a
Matilda Mared <matildamared@live.se>
2024-11-27 20:25:58 +0100

    add service to improve error messages

--- TheEmployeeAPI/Program.cs
+++ TheEmployeeAPI/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<EmployeeRepository>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
